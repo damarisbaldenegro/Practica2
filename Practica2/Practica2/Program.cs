3e7bc7ca5790f69f0cd75a960fe9e574c5cbfd07
damarisbaldenegro <damarisbaldenegro>
2014-09-30 19:28:33 -0500

    Primera version
    Signed-off-by: damarisbaldenegro <damarisbaldenegro>

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 30/09/2014
+ * Time: 06:59 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Practica2
+{
+	class Program
+	{
+		
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("dame la catidad del producto");
+			int cantipro = int.Parse (Console.ReadLine());
+		}
+	}
+}
